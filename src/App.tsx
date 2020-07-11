75fe526ccdc051e4128a66950f0cd3bdf9b5959c
jasonn0118 <tlswoals2006@gmail.com>
2020-07-11 12:13:13 -0700

    HOC concept added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component { 
+
+  render() {
+    // let firstVal: string = 'Manny';
+    // let firstVal: number = 6;
+    // let firstVal: boolean = true;
+    // let firstVal: boolean = true;
+    // let firstVal: number[] = [1,2,3,4];
+    // let firstVal: Array<string> = ['Manny', 'Mike', 'Luke'];
+
+    // Tuple
+    // let aTuple: [number, string] = [34, 'Mike'];
+
+    //enum
+    // enum Codes {first = 1, second=2};
+
+    //Any
+    // let firstName: any = 34;
+    // Note: It is not a good idea to use typeScript
+
+    // Function return
+    // const warning = (): void => {
+    //   console.log('Warning!');
+    // }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message />
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
